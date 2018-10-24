7e7a7654e2ccd4544b4477ce2478de185b91cf88
nadjacl <44436287+nadjacl@users.noreply.github.com>
2018-10-24 10:17:45 -0300

    Update minasnocampo.cpp

--- minasnocampo.cpp
+++ minasnocampo.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 int main (){
-    char mina[MAX][MAX];
+    char mina[MAX][MAX], minabomba[MAX][MAX], comando;
+
+    int x, y, contM=0;
+
     for (int i=1; i<=8; i++){
         for (int j=1; j<=8; j++){
            mina[i][j]='.';
@@ -16,18 +19,64 @@ int main (){
         }
         cout<<endl;
     }
-    /*Criar contador de minas que foram marcadas;
+    cout<<endl;
+    cout<<endl;
+    //Criar contador de minas que foram marcadas;
+    do{
+      cout<<endl;
 
-      D - - - > Descobrir o quadrado (colocar as coordenadas);
-      M - - - > Marcar mina;
-      T - - - > Talvez mina;
-      L - - - > Limpar marcação;
-      S - - - > Sair do jogo;
+      cout<<"D - - - > Descobrir o quadrado (colocar as coordenadas)"<<endl;
+      cout<<"M - - - > Marcar mina"<<endl;
+      cout<<"T - - - > Talvez mina;"<<endl;
+      cout<<"L - - - > Limpar marcação"<<endl;
+      cout<<"S - - - > Sair do jogo"<<endl;
 
-    */
+      cout<<endl;
 
+      cout<<"Insira o comando ";
+      cin>>comando;
 
+      if (comando!='S'){
+      //coordenadas
+      cin>>x>>y;
+      cout<<endl;
+      }
+      /*else{
+      //acabar o jogo;
+      }*/
 
+    for (int i=1; i<=8; i++){
+        for (int j=1; j<=8; j++){
+            switch (comando){
+            case 'D':
+                mina[x][y];
+                break;
+            case 'M':
+                mina[x][y]='M';
+                contM++;
+                break;
+            case 'T':
+                mina[x][y]='?';
+                break;
+            case 'L':
+                mina[x][y]='.';
+                contM--;
+                break;
+            }
+        }
+    }
+
+    cout<<endl;
+
+    for (int i=1; i<=8; i++){
+        cout<<i<<" ";
+        for (int j=1; j<=8; j++){
+            cout<<mina[i][j];
+        }
+        cout<<endl;
+    }
+
+}while(comando!='S' || comando!='s');
 
 
    return 0;
