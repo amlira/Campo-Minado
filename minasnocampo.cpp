e45bf556000b052c3baa80be6c1c7a152a01daf6
amlira <30601882+amlira@users.noreply.github.com>
2018-11-02 00:01:39 -0300

    Update minasnocampo.cpp
    Imprimindo bombas;
    Imprimindo quantidade de bombas na vizinhança de cada celula.

--- minasnocampo.cpp
+++ minasnocampo.cpp
@@ -4,16 +4,29 @@
 #define MAX 100
 using namespace std;
 
-void imprimir(char v[MAX][MAX]) {
+//funcao para imprimir array de string
+void imprimir (char v[MAX][MAX]) {
     for (int i=1; i<=8; i++){
-        cout<<i<<" ";
+        cout<<i<<"   ";
         for (int j=1; j<=8; j++){
-            cout<<v[i][j];
+            cout<<v[i][j]<<" ";
         }
         cout<<endl;
     }
 }
 
+//funcao para imprimir array de inteiros
+void imprimirInt (int w[MAX][MAX]) {
+    for (int i=1; i<=8; i++){
+        cout<<i<<"   ";
+        for (int j=1; j<=8; j++){
+            cout<<w[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+//funcao para usar letras maiusculas ou minusculas
 void letras (char &c) {
     if (96<c && c<123) {
         for(int i=0; i<256; i++) {
@@ -22,30 +35,97 @@ void letras (char &c) {
     }
 }
 
-int main (){
-    char mina[MAX][MAX], minabomba[MAX][MAX], comando;
+//funcao para transformar int em char
+char inTChar(int i) {
+    switch (i) {
+           case 0: return '0';
+           case 1: return '1';
+           case 2: return '2';
+           case 3: return '3';
+           case 4: return '4';
+           case 5: return '5';
+           case 6: return '6';
+           case 7: return '7';
+           case 8: return '8';
+    }
+}
 
+//funcao para imprimir a localizacao das bombas ao final
+void ImprimeBombas(int v[MAX][MAX], char w[MAX][MAX]){
+		for(int i=1; i<=10; i++){
+			for(int j=1; j<=10; j++){
+				if(v[i][j]==9){
+					w[i][j]='*';
+				}
+				else {
+					w[i][j]=inTChar(v[i][j]);
+				}
+			}
+		}
+    imprimir(w);
+    system("pause");
+}
+
+int main (){
+    char mina[MAX][MAX], comando;
+	int minabomba[MAX][MAX];
     int x, y, c, l, contM=10, aux=0, contNum=0, contBomba=1;
 
     for (int i=1; i<=8; i++){
         for (int j=1; j<=8; j++){
-            minabomba[i][j]='.';
             mina[i][j]='.';
         }
     }
 
     srand(time(NULL));
 
-    while (contBomba <= 10) {
+    //colando bombas aleatoriamente pela matriz
+	while (contBomba <= 10) {
         l=1+rand()%8;
         c=1+rand()%8;
-        if (minabomba[l][c] != '*') {
-            minabomba[l][c]= '*';
+        if (minabomba[l][c] != 9) {
+            minabomba[l][c]= 9;
             contBomba++;
         }
     }
+    
+    //verificar se ha bombas nas celulas vizinhas
+    for (int i=1; i<=8; i++){
+        for (int j=1; j<=8; j++){
+        	contNum=0;
+			if (minabomba[i-1][j-1] == 9) {
+				contNum++;
+			}
+			if (minabomba[i-1][j] == 9) {
+				contNum++;
+			}
+			if (minabomba[i-1][j+1] == 9) {
+				contNum++;
+			}
+			if (minabomba[i][j-1] == 9) {
+				contNum++;
+			}
+			if (minabomba[i][j+1] == 9) {
+				contNum++;
+			}
+			if (minabomba[i+1][j-1] == 9) {
+				contNum++;
+			}
+			if (minabomba[i+1][j] == 9) {
+				contNum++;
+			}
+			if (minabomba[i+1][j+1] == 9) {
+				contNum++;
+			}
+			
+			//colocando numero de bombas dentro da celula
+			if (minabomba[i][j] != 9) {
+				minabomba[i][j]=contNum;
+			}
+        }
+    }
 
-    imprimir(minabomba);
+    imprimirInt(minabomba);
     cout<<"\n"<<"\n"<<endl;
 
     while(aux!=1){
@@ -56,8 +136,8 @@ int main (){
         cout<<"Minas a marcar: "<<contM<<endl;
         cout<<"D - - - > Descobrir o quadrado (colocar as coordenadas)"<<endl;
         cout<<"M - - - > Marcar mina"<<endl;
-        cout<<"T - - - > Talvez mina;"<<endl;
-        cout<<"L - - - > Limpar marcação"<<endl;
+        cout<<"T - - - > Talvez mina"<<endl;
+        cout<<"L - - - > Limpar marcacao"<<endl;
         cout<<"S - - - > Sair do jogo"<<endl;
 
         cout<<endl;
@@ -76,18 +156,25 @@ int main (){
             for (int j=1; j<=8; j++){
                 switch (comando){
                 case 'D':
-                    mina[x][y];
+                    if(minabomba[x][y] != 9) {
+					mina[x][y] = inTChar(minabomba[x][y]);
+					}
+                    if(minabomba[x][y] == 9){
+                    	ImprimeBombas(minabomba, mina);
+					}
                     break;
                 case 'M':
                     mina[x][y]='M';
-                    contM--;
+                    /* Esse contador ta dando super errado
+					contM--;*/
                     break;
                 case 'T':
                     mina[x][y]='?';
                     break;
                 case 'L':
                     mina[x][y]='.';
-                    contM++;
+                    /* Ta dando errado em todo canto
+					contM--;*/
                     break;
                 case 'S':
                     aux=1;
